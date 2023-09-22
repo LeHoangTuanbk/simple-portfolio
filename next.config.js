1e9b700f41c4f65fd99a4f8e22917c565c96f4fc
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2023-09-22 18:26:16 +0900

    intro:image, text

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname:'newsroom.churchofjesuschrist.org'
+            }
+        ]
+    }
+
+}
 
 module.exports = nextConfig
