31c81f7dcef2c2ad21e01ad05c1a075970b4743e
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2023-09-25 22:15:59 +0900

    add about me

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-export default function SectionHeading() {
+type SectionHeadingProps = {
+    children: React.ReactNode;
+}
+
+export default function SectionHeading({children}: SectionHeadingProps) {
   return (
-    <div>SectionHeading</div>
+    <h2 className='text-3xl font-medium capitalize mb-8'>{children}</h2>
   )
 }
