f506d4e2400194e52d7b6547ae070a6aa03220a4
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2023-09-28 13:07:26 +0900

    add dark mode

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -6,9 +6,12 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
+import { useTheme } from '@/context/theme-context';
 
 export default function Experience() {
     const { ref } = useSectionInView({ sectionName: "Experience", threshold: 0.5 });
+    const { theme } = useTheme();
+
     return (
         <section id="experience" ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
             <SectionHeading>
@@ -19,7 +22,7 @@ export default function Experience() {
                     <VerticalTimelineElement
                         key={index}
                         contentStyle={{
-                            background: "#f3f4f6",
+                            background: theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
                             boxShadow: "none",
                             border: "1px solid rgba(0,0,0,0.05",
                             textAlign: 'left',
@@ -27,19 +30,19 @@ export default function Experience() {
                         }}
 
                         contentArrowStyle={{
-                            borderRight: "0.4rem solid #9ca3af"
+                            borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5"
                         }}
 
                         date={item.date}
                         icon={item.icon}
                         iconStyle={{
-                            background: "light",
+                            background: theme === "light" ? "white" : "rgba(255,255,255,0.15",
                             fontSize: "1.5rem"
                         }}
                     >
                         <h3 className='font-semibold capitalize'>{item.title}</h3>
                         <p className='font-normal !mt-0'>{item.location}</p>
-                        <p className='!mt-1 !font-normal text-gray-700'>{item.description}</p>
+                        <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{item.description}</p>
                     </VerticalTimelineElement>
                 ))}
             </VerticalTimeline>
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -71,14 +71,14 @@ export default function Intro() {
                 <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
                     Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />{" "}
                 </Link>
-                <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 '
+                <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10'
                     href="/CV.pdf" download>
                     Download CV <HiDownload className='opacity-60 group-hover:translate-x-1 transition' />
                 </a>
-                <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10' href="https://www.linkedin.com/in/le-hoang-tuan-bk/" target="_blank">
+                <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60' href="https://www.linkedin.com/in/le-hoang-tuan-bk/" target="_blank">
                     <BsLinkedin />
                 </a>
-                <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10 ' href="https://github.com/LeHoangTuanbk" target="_blank">
+                <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10 dark:text-white/60 ' href="https://github.com/LeHoangTuanbk" target="_blank">
                     <FaGithubSquare />
                 </a>
             </motion.div>
